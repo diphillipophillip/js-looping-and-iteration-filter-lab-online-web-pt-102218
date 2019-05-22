1d4b5bf430bc6f80a74e87ccdb0955471650e100
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-05-22 13:41:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file
+
+function findMatching(drivers, name) {
+  return drivers.filter(function(driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter
+}
