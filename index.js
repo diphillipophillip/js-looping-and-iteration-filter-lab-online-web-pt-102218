c5a6c536b1a5c17b64b97769f058c1d2742a2801
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-05-22 13:50:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function fuzzyMatch(drivers, name) {
 }
 
 function matchName(drivers, name) {
- return drivers.filter(function(){
+ return drivers.filter(function(driverName){
    return drivers.name === name
  })
 }
